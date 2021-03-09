a4b5c5b2388132051828aa9310f43b43187f3ed5
朱兴平 <18334050440@163.com>
2021-03-09 11:26:39 +0800

    feat(null): 日期转换器
    将日期格式转换成前端需要的类型

---
+++ src/main/java/xyz/tpvillage/common/DateConverter.java
@@ -0,0 +1,32 @@
+package xyz.tpvillage.common;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author: 朱兴平
+ * @date: 2020/11/10
+ */
+@Component
+public class DateConverter implements Converter<String,Date> {
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    private final String EMPTY_DATE_STRING = "0000-00-00 00:00:00";
+    @Override
+    public Date convert(String s) {
+        System.out.println("Hello s = "+s);
+        Date date = null;
+        if(EMPTY_DATE_STRING.equals(s)){
+            return null;
+        }
+        try {
+            date = simpleDateFormat.parse(s);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+}
